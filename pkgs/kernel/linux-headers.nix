d079d024fc21c53ba1d3aa9fb715f3fc5a6b63b5
Louis Opter <louis@andyl.com>
2026-05-01 01:40:56 +0000

    Bump linux to 6.18.26 (copy.fail) & stop hardcoding linux-headers to x86

--- pkgs/kernel/linux-headers.nix
+++ pkgs/kernel/linux-headers.nix
@@ -2,46 +2,55 @@
 {
   mkDerivation,
   linuxSource,
+  stdenv,
   gnumake,
-}:
-mkDerivation {
-  pname = "linux-headers";
-  inherit (linuxSource) version src;
-
-  buildDeps = [gnumake];
-  runtimeDeps = [];
-  propagatedDeps = [];
-
-  phases = [
-    {
-      name = "unpack";
-      script = ''
-        tar xf $src
-        cd linux-${linuxSource.version}
-      '';
-    }
-    {
-      name = "install";
-      script = ''
-        # Use 'make headers' to sanitize headers in-tree, then copy manually.
-        # This avoids 'make headers_install' which requires rsync, a tool not
-        # present in the bootstrap toolchain.
-        make headers ARCH=x86
-
-        mkdir -p $out/include
-        cp -r usr/include/* $out/include/
-
-        # Remove extraneous files left by the kernel build system
-        find $out/include -name '*.install.cmd' -delete
-        find $out/include -name '..install.cmd' -delete
-        find $out/include -name '.install' -delete
-      '';
-    }
-  ];
-
-  meta = {
-    description = "Linux kernel headers for userspace compilation";
-    homepage = "https://www.kernel.org";
-    license = "GPL-2.0-only";
+}: let
+  archMap = {
+    "x86_64-linux" = {karch = "x86_64";};
+    "aarch64-linux" = {karch = "arm64";};
   };
-}
+  kernelArch =
+    archMap.${stdenv.system}
+    or (throw "linux-headers: unsupported system '${stdenv.system}'");
+in
+  mkDerivation {
+    pname = "linux-headers";
+    inherit (linuxSource) version src;
+
+    buildDeps = [gnumake];
+    runtimeDeps = [];
+    propagatedDeps = [];
+
+    phases = [
+      {
+        name = "unpack";
+        script = ''
+          tar xf $src
+          cd linux-${linuxSource.version}
+        '';
+      }
+      {
+        name = "install";
+        script = ''
+          # Use 'make headers' to sanitize headers in-tree, then copy manually.
+          # This avoids 'make headers_install' which requires rsync, a tool not
+          # present in the bootstrap toolchain.
+          make headers ARCH=${kernelArch.karch}
+
+          mkdir -p $out/include
+          cp -r usr/include/* $out/include/
+
+          # Remove extraneous files left by the kernel build system
+          find $out/include -name '*.install.cmd' -delete
+          find $out/include -name '..install.cmd' -delete
+          find $out/include -name '.install' -delete
+        '';
+      }
+    ];
+
+    meta = {
+      description = "Linux kernel headers for userspace compilation";
+      homepage = "https://www.kernel.org";
+      license = "GPL-2.0-only";
+    };
+  }
