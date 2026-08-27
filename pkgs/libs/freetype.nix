6de2fd5cd6fc5a59acc167580203cd526fb1be53
Dylan Plecki <dylan@andyl.com>
2026-08-27 09:06:08 +0000

    Use native FreeType API generator compiler

--- pkgs/libs/freetype.nix
+++ pkgs/libs/freetype.nix
@@ -6,8 +6,10 @@
   zlib,
   bash,
   stdenv,
+  buildPackages,
 }: let
   version = "2.13.3";
+  isDarwinCross = stdenv.isCross && stdenv.hostPlatform.isDarwin;
 in
   mkDerivation {
     pname = "freetype";
@@ -40,18 +42,55 @@ in
       }
       {
         name = "build";
-        script = ''
-          $CONFIG_SHELL ./configure \
-            $configureFlags \
-            --prefix=$out \
-            --enable-freetype-config \
-            --with-zlib=yes \
-            --without-bzip2 \
-            --without-png \
-            --without-harfbuzz \
-            --without-brotli
-          make -j$NIX_BUILD_CORES
-        '';
+        script =
+          if isDarwinCross
+          then ''
+            # FreeType builds apinames for the Linux build machine. Isolate
+            # that compiler from the surrounding target SDK and arm64-only
+            # PAC hardening before passing it through upstream's CC_BUILD.
+            native_cc=${buildPackages.cc}/bin/cc
+            mkdir -p .aos-build-tools
+            cat > .aos-build-tools/cc-for-build <<EOF
+            #!$CONFIG_SHELL
+            native_hardening=
+            for token in \$AOS_HARDENING_ENABLE; do
+              case "\$token" in
+                pacret) ;;
+                *) native_hardening="\$native_hardening \$token" ;;
+              esac
+            done
+            export AOS_HARDENING_ENABLE="\$native_hardening"
+            unset AOS_TARGET_ARCH AOS_TARGET_PLATFORM
+            unset C_INCLUDE_PATH CPLUS_INCLUDE_PATH LIBRARY_PATH
+            unset MACOSX_DEPLOYMENT_TARGET NIX_CFLAGS_COMPILE NIX_LDFLAGS SDKROOT
+            exec "$native_cc" "\$@"
+            EOF
+            chmod +x .aos-build-tools/cc-for-build
+
+            CC_BUILD="$PWD/.aos-build-tools/cc-for-build" \
+              $CONFIG_SHELL ./configure \
+                $configureFlags \
+                --prefix=$out \
+                --enable-freetype-config \
+                --with-zlib=yes \
+                --without-bzip2 \
+                --without-png \
+                --without-harfbuzz \
+                --without-brotli
+            make -j$NIX_BUILD_CORES
+          ''
+          else ''
+            $CONFIG_SHELL ./configure \
+              $configureFlags \
+              --prefix=$out \
+              --enable-freetype-config \
+              --with-zlib=yes \
+              --without-bzip2 \
+              --without-png \
+              --without-harfbuzz \
+              --without-brotli
+            make -j$NIX_BUILD_CORES
+          '';
       }
       {
         name = "install";
