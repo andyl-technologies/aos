15fe5390468ea07ef4c7d5bd97e2d717b6a3d740
Dylan Plecki <dylan@andyl.com>
2026-09-08 05:30:26 +0000

    fix(build): use native tools for Brotli cross builds

--- pkgs/compression/brotli.nix
+++ pkgs/compression/brotli.nix
@@ -2,10 +2,8 @@
 {
   mkDerivation,
   mkGithubUpstream,
-  gnumake,
-  cmake,
-  ninja,
   stdenv,
+  buildPackages,
 }: let
   upstream = mkGithubUpstream {
     unitId = "brotli-1";
@@ -51,9 +49,9 @@ in
     update = upstream.update;
 
     buildDeps = [
-      gnumake
-      cmake
-      ninja
+      buildPackages.gnumake
+      buildPackages.cmake
+      buildPackages.ninja
     ];
     runtimeDeps = [];
     propagatedDeps = [];
