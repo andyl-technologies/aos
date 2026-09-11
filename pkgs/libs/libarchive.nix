32b6cedac271f17d992095eaba842153a4f2610e
Dylan Plecki <dylan@andyl.com>
2026-09-11 09:38:26 +0000

    Prefer target compression metadata in libarchive cross builds

--- pkgs/libs/libarchive.nix
+++ pkgs/libs/libarchive.nix
@@ -2,6 +2,8 @@
 {
   mkDerivation,
   fetchurl,
+  lib,
+  stdenv,
   gnumake,
   pkg-config,
   openssl,
@@ -60,20 +62,26 @@ in
       }
       {
         name = "configure";
-        script = ''
-          ./configure \
-            $configureFlags \
-            --prefix=$out \
-            --enable-shared \
-            --disable-static \
-            --with-openssl \
-            --with-zlib \
-            --with-zstd \
-            --with-bz2lib \
-            --without-xml2 \
-            --with-expat \
-            --with-lz4
-        '';
+        script =
+          lib.optionalString (stdenv.isCross && stdenv.hostPlatform.isLinux) ''
+            # The native xz unpacker also supplies pkg-config metadata. Prefer
+            # target compression libraries when configure resolves link flags.
+            export PKG_CONFIG_PATH=${openssl}/lib/pkgconfig:${zlib}/lib/pkgconfig:${zstd}/lib/pkgconfig:${lz4}/lib/pkgconfig:${expat}/lib/pkgconfig:${xz}/lib/pkgconfig:$PKG_CONFIG_PATH
+          ''
+          + ''
+            ./configure \
+              $configureFlags \
+              --prefix=$out \
+              --enable-shared \
+              --disable-static \
+              --with-openssl \
+              --with-zlib \
+              --with-zstd \
+              --with-bz2lib \
+              --without-xml2 \
+              --with-expat \
+              --with-lz4
+          '';
       }
       {
         name = "build";
