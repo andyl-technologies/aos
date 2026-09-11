4a20a105d49f435d05033bd7aaa16948229f4e91
Dylan Plecki <dylan@andyl.com>
2026-09-11 12:48:52 +0000

    Use target kernel headers when cross compiling strace

--- pkgs/tools/strace.nix
+++ pkgs/tools/strace.nix
@@ -2,10 +2,13 @@
 {
   mkDerivation,
   fetchurl,
+  lib,
+  stdenv,
   gnumake,
   linux-headers,
 }: let
   version = "7.2";
+  isLinuxCross = stdenv.isCross && stdenv.hostPlatform.isLinux;
 in
   mkDerivation {
     pname = "strace";
@@ -18,7 +21,7 @@ in
       hash = "sha256-S95iRpJokNzugk9uasQqBnUvR9d+UJfYbjwNbUtwn+U=";
     };
 
-    buildDeps = [gnumake linux-headers];
+    buildDeps = [gnumake] ++ lib.optionals (!isLinuxCross) [linux-headers];
     runtimeDeps = [];
     propagatedDeps = [];
 
@@ -39,12 +42,18 @@ in
       }
       {
         name = "configure";
-        script = ''
-          ./configure \
-            --prefix=$out \
-            --disable-mpers \
-            --enable-static=no
-        '';
+        script =
+          lib.optionalString isLinuxCross ''
+            # Kernel declarations describe the traced target, not the build
+            # machine selected by executable build-dependency splicing.
+            export C_INCLUDE_PATH="${linux-headers}/include''${C_INCLUDE_PATH:+:$C_INCLUDE_PATH}"
+          ''
+          + ''
+            ./configure \
+              --prefix=$out \
+              --disable-mpers \
+              --enable-static=no
+          '';
       }
       {
         name = "build";
