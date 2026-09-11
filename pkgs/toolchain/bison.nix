bd9716a1b4b403be57b7e5c4eefa1a664c0a2f3e
Dylan Plecki <dylan@andyl.com>
2026-09-11 10:43:39 +0000

    Retain target M4 for Linux cross Bison parser generation

--- pkgs/toolchain/bison.nix
+++ pkgs/toolchain/bison.nix
@@ -2,6 +2,7 @@
 {
   mkDerivation,
   fetchurl,
+  lib,
   gnumake,
   m4,
   bash,
@@ -42,12 +43,18 @@ in
       }
       {
         name = "configure";
-        script = ''
-          ./configure \
-            $configureFlags \
-            --prefix=$out \
-            --disable-nls
-        '';
+        script =
+          lib.optionalString (stdenv.isCross && stdenv.hostPlatform.isLinux) ''
+            # Bison embeds this executable path for runtime parser generation.
+            # The native build dependency is scrubbed from the installed binary.
+            export M4=${m4}/bin/m4
+          ''
+          + ''
+            ./configure \
+              $configureFlags \
+              --prefix=$out \
+              --disable-nls
+          '';
       }
       {
         name = "build";
