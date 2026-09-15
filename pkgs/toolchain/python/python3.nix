c2224896f528a9947c4c3f9b5a6df4720863691b
Dylan Plecki <dylan@andyl.com>
2026-09-15 18:36:03 +0000

    Test Python extension runtime closure

--- pkgs/toolchain/python/python3.nix
+++ pkgs/toolchain/python/python3.nix
@@ -232,7 +232,6 @@ in
     checks = {
       testing,
       self,
-      pkgs,
     }: {
       import = testing.mkVMTest {
         name = "cross-cutting-python-import";
@@ -259,23 +258,25 @@ in
 
       chain = testing.mkVMTest {
         name = "cross-cutting-python-chain";
-        rootfsDeps = [
-          self
-          pkgs.sqlite
-          pkgs.zlib
-          pkgs.readline
-        ];
+        rootfsDeps = [self];
         memory = 512;
         testScript = ''
           export PATH="${self}/bin:$PATH"
-          export LD_LIBRARY_PATH="${self}/lib:${pkgs.sqlite}/lib:${pkgs.zlib}/lib:${pkgs.readline}/lib:$LD_LIBRARY_PATH"
+          export LD_LIBRARY_PATH="${self}/lib:$LD_LIBRARY_PATH"
 
           echo "==> Testing python3 C extension modules"
 
           python3 -c "
+          import _bz2
+          import _curses
+          import _curses_panel
+          import _lzma
+          import _ssl
+          import _zstd
+          import ctypes
+          import readline
           import sqlite3
           import zlib
-          import readline
           print('sqlite3: connected to', sqlite3.sqlite_version)
           db = sqlite3.connect(':memory:')
           db.execute('CREATE TABLE t(x)')
