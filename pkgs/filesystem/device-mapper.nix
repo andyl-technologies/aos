97af0c502cab18b2f4727020baeefe23583b1f52
Dylan Plecki <dylan@andyl.com>
2026-09-25 18:33:15 +0000

    Restore device-mapper RPATH post-scrub verification

--- pkgs/filesystem/device-mapper.nix
+++ pkgs/filesystem/device-mapper.nix
@@ -4,6 +4,7 @@
   fetchurl,
   gnumake,
   pkg-config,
+  buildPackages,
   libaio,
   util-linux,
 }: let
@@ -24,6 +25,9 @@ in
     buildDeps = [
       gnumake
       pkg-config
+      buildPackages.binutils
+      buildPackages.patchelf
+      buildPackages.python3
     ];
     runtimeDeps = [
       libaio
@@ -84,6 +88,16 @@ in
         '';
       }
     ];
+
+    # Nuke-refs leaves reviewed dead RPATH placeholders in libdevmapper.
+    # Remove only that exact inventory after reference scrubbing.
+    postFinalize = ''
+      ${buildPackages.python3}/bin/python3 -B \
+        ${./aos-device-mapper-rpath-sanitize.py} \
+        --patchelf ${buildPackages.patchelf}/bin/patchelf \
+        --readelf ${buildPackages.binutils}/bin/readelf \
+        --root "$out"
+    '';
 
     meta = {
       description = "Device-mapper userspace library and tools (libdevmapper, dmsetup)";
