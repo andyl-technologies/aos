52edd1c323e9e115661bb22e280af0a9a7633c54
Dylan Plecki <dylan@andyl.com>
2026-09-25 03:56:12 +0000

    Format ability package smoke provider fixture

--- pkgs/tests/ability-package-smoke-provider.nix
+++ pkgs/tests/ability-package-smoke-provider.nix
@@ -19,8 +19,19 @@
 in
   mkDerivation {
     platformSupport = {
-      build = [{abi = ["gnu"]; os = ["linux"];}];
-      host = [{abi = ["gnu"]; cpu = ["x86_64" "aarch64"]; os = ["linux"];}];
+      build = [
+        {
+          abi = ["gnu"];
+          os = ["linux"];
+        }
+      ];
+      host = [
+        {
+          abi = ["gnu"];
+          cpu = ["x86_64" "aarch64"];
+          os = ["linux"];
+        }
+      ];
       target = [];
       role = "build-input";
     };
