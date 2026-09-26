c8573e98764422dbfdbc9d1659bb301202342106
Dylan Plecki <dylan@andyl.com>
2026-09-26 14:01:05 +0000

    Prepare source-built Bazel 8 and 9 bootstraps

--- pkgs/toolchain/_bazel-source.nix
+++ pkgs/toolchain/_bazel-source.nix
@@ -3,34 +3,8 @@
   fetchgit,
   buildPackages,
 }:
-fetchgit {
-  url = "https://github.com/bazelbuild/bazel.git";
-  ref = "7.7.1";
+(import ./_bazel-source-checkout.nix {inherit fetchgit buildPackages;}) {
+  version = "7.7.1";
   rev = "f4104dcf95cc726672c9d7d80dcc2907d3d57209";
-  name = "bazel-7.7.1-source-only";
-  hash = "sha256-lrNdrwnzlv7qe9nASUGo/R10aMWtypCp+X2Swz35YoY=";
-
-  git = buildPackages.git-minimal;
-  caCertificates = buildPackages.ca-certificates;
-  coreutils = buildPackages.coreutils;
-
-  # The pinned tag tracks 46 compiled/archive fixtures. Exclude their blobs
-  # before checkout; source-built replacements enter through declared inputs.
-  sparsePatterns = [
-    "/*"
-    "!*.jar"
-    "!*.class"
-    "!*.so"
-    "!*.a"
-    "!*.o"
-    "!*.wasm"
-    "!*.dll"
-    "!*.dylib"
-    "!*.exe"
-    "!*.bin"
-    "!*.zip"
-    "!*.tar"
-    "!*.gz"
-    "!*.xz"
-  ];
+  hash = "sha256-mbf2cIqgzFQt0ADDjMM+sxjihTkaZh3sOAI4+d5ZO34=";
 }
